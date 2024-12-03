8c5407e3d86e95d211801b2ad1cfe6d82aa6ac00
Rhyzert <leoflorentino97@gmail.com>
2024-12-02 22:32:15 -0300

    PATCH USUARIo

--- src/usuario/dto/update-usuario.dto.ts
+++ src/usuario/dto/update-usuario.dto.ts
@@ -1,7 +1,30 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUsuarioDto } from './create-usuario.dto';
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsUUID, IsOptional,IsString,Length } from 'class-validator';
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
+export class UpdateUsuarioDto  {
+    @ApiProperty({ description: 'Nome do usuário', required: false })
+    @IsOptional()
+    @IsString()
+    nome?: string;
+  
+    @ApiProperty({ description: 'Sobrenome do usuário', required: false })
+    @IsOptional()
+    @IsString()
+    sobrenome?: string;
+  
+    @ApiProperty({
+      description: 'CPF do usuário',
+      required: false,
+    })
+    @IsOptional()
+    @Length(14)
+    cpf?: string;
 
+    @ApiProperty({
+        description: 'Genero',
+        required: false,
+      })
+      @IsOptional()
+      genero?: string;
 }
